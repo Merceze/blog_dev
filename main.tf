e95c9cbe8716fc2c93237fcc11f20d419f9ebe62
Merceze <37066879+Merceze@users.noreply.github.com>
2023-06-01 11:47:00 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,12 +21,31 @@ data "aws_vpc" "default" {
 resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  
+  vpc_security_group_ids = [module.blog_security_group.security_group_id]
   
   tags = {
     name = "HelloWorld"
   }
 }
+
+module "blog_security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.17.2"
+  name    = "blog_new" 
+  
+  vpc_id = data.aws_vpc.default.id
+  
+  ingress_rules        = ["http_80_tcp,https_443_tcp"]
+  ingress_cidr_blocks  = ["0.0.0.0/0"]
+  
+  engress_rules        = ["all-all"]
+  engress_cidr_blocks  = ["0.0.0.0/0"]
+  
+  http-80-tcp   = [80, 80, "tcp", "HTTP"]
+  https-443-tcp  = [443, 443, "tcp", "HTTPS"]
+}
+
 
 resource "aws_security_group" "blog" {
   name        = "blog"
