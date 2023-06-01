c993f347b7cd25b75b121df5aecba2b34c733941
Merceze <37066879+Merceze@users.noreply.github.com>
2023-06-01 10:26:24 +0200

    Update main.tf
    ajout de règles de sécurités

--- main.tf
+++ main.tf
@@ -14,11 +14,54 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+data "aws-vpc" "default" {
+  default = true  
+}
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
+  vpc_group_security_ids = [aws_security_group_blog_id]
+  
   tags = {
-    Name = "HelloWorld"
+    name = "HelloWorld"
   }
+}
+
+resource "aws_security_group" "blog" {
+  name        = "blog"
+  description = "Allow HTTP and HTTPS, and everything"
+
+  vpc.id = data.aws_vpc_default.id
+}
+
+resource "aws_security_group_rule" "blog_http_in" {
+  type        = "ingress"
+  from_port   = 80
+  to port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_https_in" {
+  type        = "ingress"
+  from_port   = 443
+  to port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_everything_out" {
+  type        = "egress"
+  from_port   = 0
+  to port     = 0
+  protocol    = "-&"
+  cidr_blocks = ["0.0.0.0/0"]
+  
+  security_group_id = aws_security_group.blog.id
 }
