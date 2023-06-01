2a4bd1d1053b241be129a7c4da11bd7880debf49
Merceze <37066879+Merceze@users.noreply.github.com>
2023-06-01 15:05:04 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "instance_ami" {
-  value = aws_instance.blog_new.ami
+  value = aws_instance.blog.ami
 }
 
 output "instance_arn" {
-  value = aws_instance.blog_new.arn
+  value = aws_instance.blog.arn
 }
