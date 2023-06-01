de94c8b7b08e7e40b025758e0aa6f8f2a49f7187
Merceze <37066879+Merceze@users.noreply.github.com>
2023-06-01 14:32:14 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-module "vpc" {
+module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "dev"
@@ -46,7 +46,7 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
 
-  vpc_id  = module.vpc.vpc_id
+  vpc_id  = module.blog_vpc.vpc_id
   name    = "blog"
   ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
