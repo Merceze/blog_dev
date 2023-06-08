bceba2139dec6e0309ebcb4931aef31fd24a97cf
Merceze <37066879+Merceze@users.noreply.github.com>
2023-06-08 11:31:21 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,41 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
 }
+
+variable "ami_filter" {
+  description = "Name filter and owner for ami"
+  
+  type = object ({
+    name  = string
+    owner = string
+  })
+  
+  default {
+    name  = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+    owner = "979382823631" # Bitnami
+  }
+}
+
+variable "environment" {
+  description = "Dev env"
+  
+  type = object ({
+    name           = string
+    network_prefix = string 
+  })
+  
+  default {
+    name = "dev"
+    network_prefix = "10.0
+  }
+}
+
+variable "min_size" {
+  description = "Min instance in asg"
+  default     = 1  
+}
+
+variable "max_size" {
+  description = "Max instance in asg"
+  default     = 2  
+}
